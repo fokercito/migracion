87cb22b858d5eae854474023cbcab04e21b801e6
fokercito <pedronoe@comunidad.unam.mx>
2018-02-16 22:39:01 -0600

    primer intento

---
+++ pruebas.py
@@ -0,0 +1,12 @@
+import bs4 as bs
+import urllib3
+import pandas as pd
+http=urllib3.PoolManager()
+url='https://www.siass.unam.mx/consulta/494290'
+r=http.request('GET',url)
+r.status
+soup=bs.BeautifulSoup(r.data,'html.parser')
+tabla=soup.find_all('tr')
+for i in tabla:
+    for a in i.find_all('td'):
+        print(a.text)
