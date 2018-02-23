83774aa1888ee7ca84661ab1c33d357acf4baae6
fokercito <pedronoe@comunidad.unam.mx>
2018-02-23 16:59:12 -0600

    diccionario creado

--- pruebas.py
+++ pruebas.py
@@ -5,10 +5,16 @@ http=urllib3.PoolManager()
 url='https://www.siass.unam.mx/consulta/494290'
 r=http.request('GET',url)
 r.status
+diccionario = {}
 soup=bs.BeautifulSoup(r.data,'html.parser')
 tabla=soup.find_all('tr')
 for i in tabla:
     for a in i.find_all('td'):
         for z in i.find_all('th'):
             print(" ".join( z.text.split()),"<----------->"," ".join( a.text.split()))
+            llave= " ".join( z.text.split())
+            valor = " ".join( a.text.split())
+            diccionario[llave] = valor
             
+print("<============Diccionario=============>")
+print(diccionario)  
