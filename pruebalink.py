20e99ea58bfd8b44aa2e459d64560a7a8029063d
fokercito <pedronoe@comunidad.unam.mx>
2018-03-03 15:16:16 -0600

    UTF8 agregado

--- pruebalink.py
+++ pruebalink.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import bs4 as bs
 import urllib3
 import pandas as pd
@@ -57,48 +58,24 @@ for e in range (2,max):
     for i in arrlinks:
         if("https://www.siass.unam.mx/consulta/" in i ):
                 numerosConsulta.append(i.replace("https://www.siass.unam.mx/consulta/",""))
-
-    for r in numerosConsulta:            
-        url2='https://www.siass.unam.mx/consulta/' + r
-        r=http.request('GET',url2)
-        r.status
-        diccionario = {}
-        soup=bs.BeautifulSoup(r.data,'html.parser')
-        tabla=soup.find_all('tr')
-        f=open("diccionario.txt","w")
-        for i in tabla:
-            for a in i.find_all('td'):
-                for z in i.find_all('th'):
-                    print(" ".join( z.text.split()),"<----------->"," ".join( a.text.split()))
-                    llave= " ".join( z.text.split())
-                    valor = " ".join( a.text.split())
-                    diccionario[llave] = valor
-        arregloDic.append(diccionario)
-        f.write(str(diccionario) + "\n")
-        f.close()        
-print(arregloDic)
-f=open("listadiccionario.txt","a")
-f.write(arregloDic)
-f.close()
-#print(numerosConsulta)
-
-
-"""
-intento de recorrer la pagina
-        url= i
-        r=http.request('GET',url)
-        r.status
-        soup=bs.BeautifulSoup(r.data,'html.parser')
-        linkstemp=soup.find_all('a')
-        for i in linkstemp:
-            linkstemporales.append(i['href'])
-        for j in linkstemporales:
-            print("entre al ciclo de link temporal")
-            if("https://www.siass.unam.mx/consulta/" in j ):  
-                print("encontre un numero de consulta")
-                numerosConsulta.append(i.replace("https://www.siass.unam.mx/consulta/","")
-print(numerosConsulta)
-#print(len(numerosConsulta))
-#print(numerosConsulta)
-
-"""
+for r in numerosConsulta:            
+    url2='https://www.siass.unam.mx/consulta/' + str(r)
+    r=http.request('GET',url2)
+    r.status
+    diccionario = {}
+    soup=bs.BeautifulSoup(r.data,'html.parser')
+    tabla=soup.find_all('tr')
+    f=open("diccionario.txt","a",encoding="utf8")
+    for i in tabla:
+        for a in i.find_all('td'):
+            for z in i.find_all('th'):
+                print(" ".join( z.text.split()),"<----------->"," ".join( a.text.split()))
+                llave= " ".join( z.text.split())
+                valor = " ".join( a.text.split())
+                diccionario[llave] = valor
+    arregloDic.append(diccionario)
+    f.write(str(diccionario) + "\n")
+    f.close()        
+g=open("listadiccionario.txt","a",encoding="utf8")
+g.write(str(arregloDic)
+g.close()
